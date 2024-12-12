11aacff89146b31e2c1ce9e8e0e4f2ab29cf077f
SeUkKim <eleccookie@gmail.com>
2024-12-12 19:04:39 +0900

    wip: test time interval.

--- informative-indexer/prunner/prunner.go
+++ informative-indexer/prunner/prunner.go
@@ -7,6 +7,9 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var logger *zerolog.Logger
@@ -31,7 +34,7 @@ type PrunnerConfig struct {
 
 func NewPrunner(config *PrunnerConfig) (*Prunner, error) {
 	logger = zerolog.Ctx(log.With().Str("component", "informative-indexer-prunner").Str("chain", config.Chain).Str("environment", config.Environment).Str("commit_sha", config.CommitSHA).Logger().WithContext(context.Background()))
-	
+
 	dbClient, err := db.NewClient(config.DBConnectionString)
 	if err != nil {
 		logger.Fatal().Msgf("DB: Error creating DB client: %v", err)
@@ -52,8 +55,40 @@ func NewPrunner(config *PrunnerConfig) (*Prunner, error) {
 
 }
 
-func (p *Prunner) Prune() {
-	logger.Log().Msg("PRUNE ENTRY")
+func (p *Prunner) StartPruning(signalCtx context.Context) {
+	logger.Info().Msg("Prunner: Starting pruning process ...")
 
-	logger.Log().Msg("PRUNE EXIT")
+	for {
+		select {
+		case <-signalCtx.Done():
+			logger.Info().Msg("Prunner: Received stop signla. Exiting pruning loop ...")
+			return
+		default:
+			height, err := db.GetLatestBlockHeight(context.Background(), p.dbClient)
+			if err != nil {
+				logger.Error().Msgf("DB: Error getting latest block height: %v", err)
+				panic(err)
+			}
+			logger.Info().Msgf("Current block height: %d", height)
+
+			// interval
+			time.Sleep(5 * time.Second)
+		}
+	}
+}
+
+func (p *Prunner) Prune() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go p.StartPruning(ctx)
+
+	<-ctx.Done()
+
+	logger.Info().Msgf("Stopping prunner ...")
+	p.close()
+}
+
+func (p *Prunner) close() {
+	p.dbClient.Close()
 }
