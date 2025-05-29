8da8f6f9bf323345b9d3c0e542a3375ff0d49b58
benzbeeb <benz@initia.xyz>
2025-05-29 13:03:03 +0700

    refactor: parse coin amt func

--- pkg/parser/coin.go
+++ pkg/parser/coin.go
@@ -4,13 +4,11 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func ParseCoinAmount(coinStr string) (amount int64, denom string, err error) {
+func ParseCoinAmount(coinStr string) (int64, string, error) {
 	coin, err := sdk.ParseCoinNormalized(coinStr)
 	if err != nil {
 		return 0, "", err
 	}
 
-	amount = coin.Amount.Int64()
-	denom = coin.Denom
-	return amount, denom, nil
+	return coin.Amount.Int64(), coin.Denom, nil
 }
