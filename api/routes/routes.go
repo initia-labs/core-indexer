f09ade73767721590eb11cf5db76e3df920f80dd
evilpeach <kanisorn.peach@gmail.com>
2025-06-09 14:35:19 +0700

    chore: add comments, fix order

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -8,7 +8,7 @@ import (
 
 // SetupRoutes configures all the routes for the API
 func SetupRoutes(app *fiber.App, dbClient *gorm.DB, bucket *blob.Bucket) {
+	SetupBlockRoutes(app, dbClient)
 	SetupNFTRoutes(app, dbClient)
 	SetupTxRoutes(app, dbClient, bucket)
-	SetupBlockRoutes(app, dbClient)
 }
