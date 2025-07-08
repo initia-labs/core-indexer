cb7296f90e53d4150b43ac600634ebe36073329a
songwongtp <16089160+songwongtp@users.noreply.github.com>
2025-07-08 18:29:44 +0700

    fix: remove unused

--- informative-indexer/flusher/types/proposal.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import "github.com/initia-labs/core-indexer/pkg/db"
-
-type StateUpdateProposal struct {
-	ID             int32
-	Status         db.ProposalStatus
-	ResolvedHeight *int32
-}
