5c0c2ce4f25f6f4da978c3efca3df1ec3993aa60
Nattharat Kondanna Wiriyakulnan <nattharat.wi@gmail.com>
2025-07-14 14:38:27 +0700

    feat: initialize event indexer service (#99)
    * feat: initialize event indexer service
    
    * feat(event-indexer): update event-indexer/.env.example
    
    * feat(event-indexer): update event-indexer/cmd/root.go
    
    ---------
    
    Co-authored-by: David Tumcharoen <42636319+traviolus@users.noreply.github.com>

---
+++ event-indexer/indexer/block_result.go
@@ -0,0 +1,146 @@
+package indexer
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	movetypes "github.com/initia-labs/initia/x/move/types"
+	"gorm.io/gorm"
+
+	"github.com/initia-labs/core-indexer/event-indexer/indexer/types"
+	"github.com/initia-labs/core-indexer/pkg/db"
+	"github.com/initia-labs/core-indexer/pkg/mq"
+	"github.com/initia-labs/core-indexer/pkg/sentry_integration"
+)
+
+func (f *Indexer) parseAndInsertMoveEvents(parentCtx context.Context, dbTx *gorm.DB, blockResults *mq.BlockResultMsg) error {
+	span, ctx := sentry_integration.StartSentrySpan(parentCtx, "parseAndInsertMoveEvents", "Parse block_results message and insert move_events into the database")
+	defer span.Finish()
+
+	moveEvents := make([]*db.MoveEvent, 0)
+	for _, tx := range blockResults.Txs {
+		if tx.ExecTxResults.Log == types.TxParseError {
+			continue
+		}
+
+		// idx ensures EventIndex is unique within each transaction.
+		idx := 0
+		for _, event := range tx.ExecTxResults.Events {
+			if event.Type == movetypes.EventTypeMove {
+				moveEvent := &db.MoveEvent{
+					BlockHeight:     blockResults.Height,
+					TransactionHash: tx.Hash,
+					EventIndex:      idx,
+				}
+				for _, attr := range event.Attributes {
+					switch attr.Key {
+					case movetypes.AttributeKeyTypeTag:
+						moveEvent.TypeTag = attr.Value
+					case movetypes.AttributeKeyData:
+						moveEvent.Data = []byte(attr.Value)
+					}
+				}
+				moveEvents = append(moveEvents, moveEvent)
+				idx++
+			}
+		}
+	}
+
+	if err := db.InsertMoveEventsIgnoreConflict(ctx, dbTx, moveEvents); err != nil {
+		logger.Error().Msgf("Error inserting move_events: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (f *Indexer) parseAndInsertFinalizeBlockEvents(parentCtx context.Context, dbTx *gorm.DB, blockResults *mq.BlockResultMsg) error {
+	span, ctx := sentry_integration.StartSentrySpan(parentCtx, "parseAndInsertFinalizeBlockEvents", "Parse block_results message and insert finalize_block_events into the database")
+	defer span.Finish()
+
+	finalizeBlockEvents := make([]*db.FinalizeBlockEvent, 0)
+	// id ensures EventIndex is unique within each block.
+	idx := 0
+	for _, event := range blockResults.FinalizeBlockEvents {
+		// Check if the last attribute key is "mode"
+		// We set mode to the value of the last attribute key "mode" here; since in CometBFT the "mode" will get
+		// append at the end.
+		// There is a case, however, where this may not apply. Ones being messages executed in the upgrade handler.
+		// Optimistically, since events emitted from the upgrade handler do not have the mode attributes appended,
+		// they also won't be passed to the function `sdk.MarkEventsToIndex`.
+		// Thus, we can safely assume they do not belong to the finalized block events set.
+
+		attrs := event.Attributes
+		if len(attrs) == 0 {
+			continue
+		}
+
+		// Check if at least one attribute has index: true
+		hasIndexedAttr := false
+		for _, attr := range attrs {
+			if attr.Index {
+				hasIndexedAttr = true
+				break
+			}
+		}
+
+		// If there are indexed attributes, enforce the "mode" key rule
+		if hasIndexedAttr {
+			if attrs[len(attrs)-1].Key != "mode" {
+				err := fmt.Errorf("expected 'mode' as the last attribute in event, but found '%s'", attrs[len(attrs)-1].Key)
+				logger.Error().Msgf("%v", err)
+				return err
+			}
+
+			mode := attrs[len(attrs)-1].Value
+
+			// Process all attributes except the last one (which should be "mode")
+			for _, attr := range attrs[:len(attrs)-1] {
+				finalizeBlockEvents = append(finalizeBlockEvents, &db.FinalizeBlockEvent{
+					BlockHeight: blockResults.Height,
+					EventKey:    fmt.Sprintf("%s.%s", event.Type, attr.Key),
+					EventValue:  attr.Value,
+					EventIndex:  idx,
+					Mode:        mode,
+				})
+				idx++
+			}
+		}
+	}
+
+	if err := db.InsertFinalizeBlockEventsIgnoreConflict(ctx, dbTx, finalizeBlockEvents); err != nil {
+		logger.Error().Msgf("Error inserting finalize_block_events: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (f *Indexer) processBlockResults(parentCtx context.Context, blockResults *mq.BlockResultMsg) error {
+	span, ctx := sentry_integration.StartSentrySpan(parentCtx, "processBlockResults", "Parse block_results message and insert tx events into the database")
+	defer span.Finish()
+
+	logger.Info().Msgf("Processing block_results at height: %d", blockResults.Height)
+
+	if err := f.dbClient.WithContext(ctx).Transaction(func(dbTx *gorm.DB) error {
+
+		if err := f.parseAndInsertMoveEvents(ctx, dbTx, blockResults); err != nil {
+			logger.Error().Int64("height", blockResults.Height).Msgf("Error inserting move_events: %v", err)
+			return err
+		}
+
+		if err := f.parseAndInsertFinalizeBlockEvents(ctx, dbTx, blockResults); err != nil {
+			logger.Error().Int64("height", blockResults.Height).Msgf("Error inserting finalize_block_events: %v", err)
+			return err
+		}
+		return nil
+	}); err != nil {
+		logger.Error().Int64("height", blockResults.Height).Msgf("Error processing block: %v", err)
+		return errors.Join(types.ErrorNonRetryable, err)
+	}
+
+	logger.Info().Int64("height", blockResults.Height).Msgf("Successfully flushed block: %d", blockResults.Height)
+
+	return nil
+}
---
+++ event-indexer/prunner/prunner.go
@@ -0,0 +1,251 @@
+package prunner
+
+import (
+	"archive/zip"
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"gorm.io/gorm"
+
+	"github.com/initia-labs/core-indexer/pkg/db"
+	"github.com/initia-labs/core-indexer/pkg/storage"
+)
+
+var logger *zerolog.Logger
+
+type Prunner struct {
+	dbClient      *gorm.DB
+	storageClient storage.Client
+	config        *PrunnerConfig
+}
+
+type PrunnerConfig struct {
+	DBConnectionString string
+	BackupBucketName   string
+	BackupFilePrefix   string
+	PruningKeepBlock   int64
+	PruningInterval    int64
+	Chain              string
+	Environment        string
+	CommitSHA          string
+}
+
+func NewPrunner(config *PrunnerConfig) (*Prunner, error) {
+	logger = zerolog.Ctx(log.With().Str("component", "event-indexer-prunner").Str("chain", config.Chain).Str("environment", config.Environment).Str("commit_sha", config.CommitSHA).Logger().WithContext(context.Background()))
+
+	dbClient, err := db.NewClient(config.DBConnectionString)
+	if err != nil {
+		logger.Fatal().Msgf("DB: Error creating DB client: %v", err)
+		return nil, err
+	}
+
+	var storageClient storage.Client
+
+	if config.Environment == "local" {
+		storageClient, err = storage.NewGCSFakeClient()
+		if err != nil {
+			logger.Info().Msgf("Local: Error creating storage client: %v\n", err)
+			return nil, err
+		}
+	} else {
+		storageClient, err = storage.NewGCSClient()
+		if err != nil {
+			logger.Fatal().Msgf("Storage: Error creating storage client: %v\n", err)
+			return nil, err
+		}
+	}
+
+	return &Prunner{
+		dbClient:      dbClient,
+		storageClient: storageClient,
+		config:        config,
+	}, nil
+}
+
+func (p *Prunner) StartPruning(signalCtx context.Context) {
+	logger.Info().Msg("Prunner: Starting pruning process ...")
+
+	for {
+		select {
+		case <-signalCtx.Done():
+			logger.Info().Msg("Prunner: Received stop signal. Exiting pruning loop ...")
+			return
+		default:
+			pruningInterval := time.Duration(p.config.PruningInterval) * 24 * time.Hour
+			//pruningInterval := 10 * time.Second // for local test
+
+			for _, table := range db.GetValidTableNames() {
+				if err := p.pruningTable(signalCtx, table); err != nil {
+					logger.Error().Msgf("Error during pruning for table %s: %v", table, err)
+				}
+			}
+
+			time.Sleep(pruningInterval)
+		}
+	}
+}
+
+func (p *Prunner) pruningTable(ctx context.Context, tableName string) error {
+	height, err := db.GetLatestBlockHeight(ctx, p.dbClient)
+	if err != nil {
+		return fmt.Errorf("DB: failed to get latest block height: %w", err)
+	}
+
+	pruningThreshold := height - p.config.PruningKeepBlock
+	if pruningThreshold <= 0 {
+		logger.Info().Msgf("Pruning not required for table %s: threshold is too low: %d", tableName, pruningThreshold)
+		return nil
+	}
+
+	logger.Info().Msgf("Pruning rows from table %s with block_height below: %d", tableName, pruningThreshold)
+
+	query, err := db.BuildPruneQuery(ctx, p.dbClient, tableName, pruningThreshold)
+	if err != nil {
+		return fmt.Errorf("DB: Failed to prepare query for table %s: %w", tableName, err)
+	}
+
+	var count int64
+	if err := query.Count(&count).Error; err != nil {
+		return fmt.Errorf("DB: Failed to count rows for table %s: %w", tableName, err)
+	}
+
+	// no rows to prune
+	if count == 0 {
+		logger.Info().Msgf("Pruning not required for table %s: no rows to prune", tableName)
+		return nil
+	}
+
+	backupFileName := fmt.Sprintf("%s-%d.zip", p.config.BackupFilePrefix, time.Now().Unix())
+	if err := streamBackupToGCS(ctx, query, p.storageClient, p.config.BackupBucketName,
+		tableName, backupFileName); err != nil {
+		return fmt.Errorf("GCS: Failed to backup data from table %s to GCS: %w", tableName, err)
+	}
+
+	if err = pruneRows(ctx, p.dbClient, tableName, pruningThreshold); err != nil {
+		return fmt.Errorf("DB: Failed to prune rows from table %s: %w", tableName, err)
+	}
+
+	logger.Info().Msg("Pruning completed ...")
+	return nil
+}
+
+// streamBackupToGCS streams query results directly to a zip file and uploads to GCS
+func streamBackupToGCS(ctx context.Context, query *gorm.DB, storageClient storage.Client, bucketName, tableName, fileName string) error {
+	// create zip
+	var buffer bytes.Buffer
+	zipWriter := zip.NewWriter(&buffer)
+
+	// create a file inside the zip
+	zipFile, err := zipWriter.Create(fmt.Sprintf("%s.json", strings.Split(fileName, ".")[0]))
+	if err != nil {
+		return fmt.Errorf("failed to create zip entry: %w", err)
+	}
+
+	// start writing the JSON array opening bracket
+	if _, err := zipFile.Write([]byte("[")); err != nil {
+		return fmt.Errorf("failed to write opening bracket: %w", err)
+	}
+
+	// process data in chunks
+	const chunkSize = 1000
+	offset := 0
+	isFirstRow := true
+
+	for {
+		// check if context is canceled
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+
+		var chunk []map[string]any
+		if err := query.Limit(chunkSize).Offset(offset).Find(&chunk).Error; err != nil {
+			return fmt.Errorf("failed to fetch chunk: %w", err)
+		}
+
+		// no more data, break the loop
+		if len(chunk) == 0 {
+			break
+		}
+
+		for _, rowData := range chunk {
+			if !isFirstRow {
+				if _, err := zipFile.Write([]byte(",")); err != nil {
+					return fmt.Errorf("failed to write comma: %w", err)
+				}
+			} else {
+				isFirstRow = false
+			}
+
+			rowJSON, err := json.Marshal(rowData)
+			if err != nil {
+				return fmt.Errorf("failed to marshal row: %w", err)
+			}
+
+			if _, err := zipFile.Write(rowJSON); err != nil {
+				return fmt.Errorf("failed to write row JSON: %w", err)
+			}
+		}
+
+		offset += len(chunk)
+
+		if len(chunk) < chunkSize {
+			break
+		}
+	}
+
+	if _, err := zipFile.Write([]byte("]")); err != nil {
+		return fmt.Errorf("failed to write closing bracket: %w", err)
+	}
+
+	if err := zipWriter.Close(); err != nil {
+		return fmt.Errorf("failed to close zip writer: %w", err)
+	}
+
+	objectName := fmt.Sprintf("%s/%s", tableName, fileName)
+	err = storageClient.UploadFile(bucketName, objectName, buffer.Bytes())
+	if err != nil {
+		return fmt.Errorf("failed to upload file to GCS: %w", err)
+	}
+
+	return nil
+}
+
+func pruneRows(ctx context.Context, dbClient *gorm.DB, tableName string, pruningThreshold int64) error {
+	err := db.DeleteRowsToPrune(ctx, dbClient, tableName, pruningThreshold)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *Prunner) Prune() {
+	// Mutex to avoid multiple prunner instances
+	var once sync.Once
+	once.Do(func() {
+		ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+		defer stop()
+
+		go p.StartPruning(ctx)
+
+		<-ctx.Done()
+
+		logger.Info().Msgf("Stopping prunner ...")
+		p.close()
+	})
+}
+
+func (p *Prunner) close() {
+	sqlDB, err := p.dbClient.DB()
+	if err == nil {
+		sqlDB.Close()
+	}
+}
