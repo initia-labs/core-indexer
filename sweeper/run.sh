84719666f67ef23c1ae239f7314fc2888b4a923d
Nattharat Kondanna Wiriyakulnan <nattharat.wi@gmail.com>
2025-07-14 10:09:21 +0700

    feat: add sweeper functionality (#96)
    - Introduced a new Dockerfile for the sweeper service, enabling building and running the application in a containerized environment.
    - Created main entry point for the sweeper application.
    - Implemented command structure using Cobra for sweeping blockchain data.
    - Added run script to facilitate execution with necessary parameters.
    - Established configuration and logging for the sweeper service, integrating with Kafka and Sentry for message handling and error tracking.
    - Included Go module files for dependency management.

---
+++ sweeper/run.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$(cd $(dirname $0) ; pwd -P)
+
+TASK=$1
+ARGS=${@:2}
+
+help__sweep="sweep <..args> : run sweep"
+task__sweep() {
+  local chain=$1
+
+  if [ -z "$chain" ]; then
+    echo "usage: $0 sweep <chain>"
+    exit
+  fi
+
+  go build -o sweeper.bin .
+
+  source .env
+
+  ./sweeper.bin sweep --bootstrap-server $KAFKA_BOOTSTRAP_SERVER \
+    --block-results-topics ${chain}-local-informative-indexer-block-results-messages,${chain}-local-generic-indexer-block-results-messages \
+    --kafka-api-key $KAFKA_API_KEY \
+    --kafka-api-secret $KAFKA_API_SECRET \
+    --claim-check-bucket ${chain}-local-informative-indexer-large-block-results \
+    --claim-check-threshold-mb 1 \
+    --db $DB_CONNECTION_STRING \
+    --chain $chain \
+    --rebalance-interval $REBALANCE_INTERVAL \
+    --workers 4 \
+    --migrations-dir ../db/migrations
+}
+
+
+list_all_helps() {
+  compgen -v | egrep "^help__.*"
+}
+
+NEW_LINE=$'\n'
+if type -t "task__$TASK" &>/dev/null; then
+  task__$TASK $ARGS
+else
+  echo "usage: $0 <task> [<..args>]"
+  echo "task:"
+
+  HELPS=""
+  for help in $(list_all_helps)
+  do
+
+    HELPS="$HELPS    ${help/help__/} |-- ${!help}$NEW_LINE"
+  done
+
+  echo "$HELPS" | column -t -s "|"
+  exit
+fi
