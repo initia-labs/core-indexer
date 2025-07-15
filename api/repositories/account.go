d17a11076fff5676237c34118d06d3b40d6d5068
evilpeach <kanisorn.peach@gmail.com>
2025-07-15 14:43:17 +0700

    remove typo

--- api/repositories/account.go
+++ api/repositories/account.go
@@ -47,7 +47,7 @@ func (r *AccountRepository) GetAccountProposals(pagination dto.PaginationQuery, 
 			proposals.status,
 			proposals.voting_end_time,
 			proposals.deposit_end_time,
-			proposals.type,a
+			proposals.type,
 			proposals.id,
 			proposals.is_expedited,
 			proposals.is_emergency,
